c45081ae17907d72ca6b6a5dd75fb99878ed18ea
Alex Warren <alexander.ch.warren@gmail.com>
2022-10-10 18:11:01 -0400

    Add more tests

--- components/tictactoe_model.py
+++ components/tictactoe_model.py
@@ -1,2 +1,76 @@
 class TicTacToe_Model:
-    pass
+    __X: str = "X"
+    __O: str = "O"
+    __PLAYERS = [__O, __X]
+
+    @property
+    def X(self):
+        return TicTacToe_Model.__X
+
+    @property
+    def O(self):
+        return TicTacToe_Model.__O
+
+    def __init__(self):
+        self.__turn: int = 0
+        self.__current_player: str = ""
+        self.__grid: list[list[int]] = []
+        self.__winner: str = ""
+        self.reset()
+
+    @property
+    def current_player(self):
+        return self.__current_player
+
+    @property
+    def grid(self):
+        return self.__grid
+
+    @property
+    def winner(self):
+        return self.__winner
+
+    def update_grid(self, row: int, col: int):
+        self.__grid[row][col] = self.__current_player
+        self.__check_winner()
+        self.__next_player()
+
+    def reset(self):
+        self.__turn = 0
+        self.__next_player()
+        self.__clear_grid()
+        self.__winner = ""
+
+    def __clear_grid(self):
+        self.__grid = [["" for _ in range(3)] for _ in range(3)]
+    
+    def __check_winner(self):
+        def check_winner_horizontally():
+            for player in TicTacToe_Model.__PLAYERS:
+                for row in self.__grid:
+                    if all(cell == player for cell in row):
+                        self.__winner = player
+                        return True
+            return False
+
+        def check_winner_vertically():
+            for player in TicTacToe_Model.__PLAYERS:
+                for column in range(len(self.__grid)):
+                    if all(row[column] == player for row in self.__grid):
+                        self.__winner = player
+                        return True
+            return False
+
+        def check_winner_diagonally():
+            return False
+        
+        if check_winner_horizontally():
+            return
+        if check_winner_vertically():
+            return
+        if check_winner_diagonally():
+            return
+
+    def __next_player(self):
+        self.__turn += 1
+        self.__current_player = self.__PLAYERS[self.__turn % len(self.__PLAYERS)]
--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -36,3 +36,65 @@ def test_app_controller_view(app):
 def test_app_view_controller(app):
     assert isinstance(app.view.controller, controller.TicTacToe_Controller)
     assert app.view.controller is app.controller
+
+
+def test_model_players(app):
+    assert app.model.X == "X"
+    assert app.model.O == "O"
+
+
+def test_model_starting_player(app):
+    assert app.model.current_player == "X"
+
+
+def test_model_3x3_grid(app):
+    assert len(app.model.grid) == 3
+    assert all(len(row) == 3 for row in app.model.grid)
+
+
+def test_model_start_empty_grid(app):
+    assert all(all(cell == "" for cell in row) for row in app.model.grid)
+
+
+def test_model_first_turn(app):
+    app.model.update_grid(0, 1)
+    assert app.model.grid[0][1] == "X"
+    assert app.model.current_player == "O"
+
+
+@pytest.mark.parametrize(
+    "row, column, result",
+    [(1, 1, "O"), (2, 2, "X"), (0, 0, "O"), (1, 0, "X")],
+)
+def test_model_more_turns(row: int, column: int, result: str, app):
+    app.model.update_grid(row, column)
+    assert app.model.grid[row][column] == result
+
+
+def test_model_no_winner(app):
+    assert app.model.winner == ""
+
+
+def test_model_reset(app):
+    assert any(any(cell != "" for cell in row) for row in app.model.grid)
+    app.model.reset()
+    assert all(all(cell == "" for cell in row) for row in app.model.grid)
+    assert app.model.current_player == 'X'
+
+def play_to_win(app, plays, winner):
+    app.model.reset()
+    for row,column in plays:
+        app.model.update_grid(row, column)
+    assert app.model.winner == winner
+
+def test_model_X_wins_horizontally_first_row(app):
+    plays = [(0,0), (1,0), (0,1), (1,1), (0,2)]
+    play_to_win(app, plays, 'X')
+
+def test_model_O_wins_vertically_second_column(app):
+    plays = [(0,0), (0,1), (1,0), (1,1), (0,2), (2,1)]
+    play_to_win(app, plays, 'O')
+
+def test_model_X_wins_diagonally_topleft_bottomright(app):
+    plays = [(0,0), (0,1), (1,1), (2,1), (2,2)]
+    play_to_win(app, plays, 'X')
